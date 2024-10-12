293e5af83173bb60976f10f7fa20ee69b4c4162b
atul120212 <atulsharma28092004@gmail.com>
2024-10-12 23:27:19 +0530

    update-team-model

--- webpages/models.py
+++ webpages/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Team(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    role =  models.CharField(max_length=255)
+    insta_link = models.CharField(max_length=255)
+    linkedin_link = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to = 'media/team/%Y/%m/%d/')
+
+    def __str__(self):
+        return self.first_name
+
 
 class Slider(models.Model):
     headline = models.CharField(max_length=255)
